c09162d84029d5ec79743dbf15043cd4479c5eea
shirleycr2018 <62478007+shirleycr2018@users.noreply.github.com>
2022-10-10 09:38:44 -0600

    Practica de Estructuras de Datos en R
    - Vectores
    - Matrix
    - DataFrame

---
+++ Lista de Ejercicios de Tarea.R
@@ -0,0 +1,88 @@
+#Ejercicios por Resolver Parte I 
+
+#Ejercicio Parte I - Estructurqs de datos 
+---------------
+#1. Vectores
+---------------
+#1.1 Cree un vector vacio
+
+#1.2 Agregue al vector una secuencia de 1:6 
+
+# 1.2.1 Obtenga la posicion 3 del vector
+
+# 1.2.2 Obtenga todo el vector menos la posicion 5
+
+#1.2.3 Modifique el valor de la posicion 2 del vector con un valor de 5
+
+  
+# Utilizando la function append agregue al vector una sequencia de numeros que vaya del
+# 8 al 16 
+# Ejempo a=append(a,c(1:10))
+
+
+#1.3 Cree un nuevo vector que contenga los siguientes nombres:
+# Ana, Ariel, Jose, Wesly, Andrea, Silvia
+
+# Obtenga e imprima la posicion 1 del vector
+
+
+# Modifique la posicion 3 del vector con el valor "Jaime" 
+
+#1.4 Cree un vector que contenga una sequencia de numeros del 25 al 50 con incrementos de 0.75 
+
+#1.5 Calcular la Suma, la media y la mediana del vector creado en el punto anterior
+
+#1.6 Aplicar la division por 2 a los elementos del vector creado en el punto 1.4 
+
+#.....................
+# Matrices
+#.....................
+
+
+# 2.1 Cree una matriz de Dos renglones y seis columnas que contenga los numeros del 1 al 2 
+
+
+# 2.2 cree una matriz de 3 renglones y 3 columnas que contenga los numeros del 1 al 12 
+
+# 2.3 Cree una matriz de 5 renglones y 4 columnas que contenga los numeros del 1 al 12
+
+
+# 2.4 Observe y Compare el contenido y comportamiento de las tres matrices creadas anteriomente
+
+
+
+#.....................
+# Dataframe 
+
+
+#1.Para este ejercicio vamos a explorar el dataset iris que viene cargado en R por defecto 
+#.....................
+
+# Cuales son las dimienciones del dataframe iris
+
+# Como obtengo el resumen estadistico del dataframe iris (ejemplo: mean, max, mean de cada columna, etc)
+
+# cual es el nombre y el tipo de cada una de las columnas del dataframe iris
+
+
+# Obtenga los primeros 5 renglones del dataframe iris
+
+# obtenga los 5 ultimos renglones del dataframe iris
+
+
+#2.Para este ejercicio vamos a usar el dataset HairEyeColor  
+
+HairEyeColorDF <- as.data.frame(HairEyeColor)
+
+# analice la estructura del dataframe con funcion str(HairEyeColorDF)
+
+# obtenga el numero de filas del dataframe HairEyeColorDF
+
+# objecta el numero de columnas del dataframe HairEyeColorDF
+
+# cambie el nombre de la primera columna del dataframe HairEyeColorDF  
+
+
+
+
+
